31bba9ba01df8592f8c1e47c3847c495b4152b1a
GravityTwoG <crytekov@gmail.com>
2024-06-25 13:04:15 +0700

    feat: migrations

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,29 +1,48 @@
 package main
 
 import (
-	"go-jwt-auth/internal/rest-api/config"
 	"go-jwt-auth/internal/rest-api/database"
 	"go-jwt-auth/internal/rest-api/models"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	cfg := config.MustLoadConfig()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	db, err := database.ConnectToDB(cfg.DSN)
+	dsn := os.Getenv("DSN")
+	if dsn == "" {
+		log.Fatal("Invalid DSN value in .env file")
+	}
+
+	db, err := database.ConnectToDB(dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = db.AutoMigrate(&models.User{})
-	if err != nil {
-		log.Fatal(err)
+	// initial migrations
+	if !db.Migrator().HasTable(&models.User{}) {
+		log.Println("Creating users table")
+		err = db.Migrator().CreateTable(&models.User{})
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	err = db.AutoMigrate(&models.RefreshToken{})
-	if err != nil {
-		log.Fatal(err)
+	if !db.Migrator().HasTable(&models.RefreshToken{}) {
+		log.Println("Creating refresh tokens table")
+		err = db.Migrator().CreateTable(&models.RefreshToken{})
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+
+	// future migrations
 
 	log.Println("Migrations complete")
 }
