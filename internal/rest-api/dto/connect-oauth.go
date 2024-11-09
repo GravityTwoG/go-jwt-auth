cb3ebd46fbb5b676790479a182a0524bfea89fa1
GravityTwoG <crytekov@gmail.com>
2024-11-09 21:55:26 +0700

    fix: deviceId is optional

--- internal/rest-api/dto/connect-oauth.go
+++ internal/rest-api/dto/connect-oauth.go
@@ -3,7 +3,7 @@ package dto
 type ConnectOAuthDTO struct {
 	Code         string `json:"code" binding:"required"`
 	CodeVerifier string `json:"codeVerifier" binding:"required"`
-	DeviceID     string `json:"deviceID" binding:"required"`
+	DeviceID     string `json:"deviceId"`
 	RedirectURL  string `json:"redirectURL" binding:"required"`
 }
 
